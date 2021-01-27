85755e21ad8a015c7408f1a8e38ccf622eb31a78
Daniel Trone <danieljtrone@gmail.com>
2021-01-27 07:20:08 -0600

    implement bst search

--- src/data/binary-search-tree/38-search.js
+++ src/data/binary-search-tree/38-search.js
@@ -36,20 +36,104 @@ const solution = [
 
 export const data = {
   problemID: 38,
-  problemName: 'Implement `search` in a Binary Search Tree',
+  problemName: 'Implement **search** in a *BinarySearchTree* class.',
   problemText:
-    'Write a **search** method in a BinarySearchTree Class that accepts an integer value and returns a boolean true or false indicating whether that integer is found within the tree.',
+    'Write a **search** method in a *BinarySearchTree* class that accepts a `value` (integer) and returns a boolean `true` or `false` indicating whether that integer is found within the tree.',
   testCases: [
     {
       id: 1,
       name: 'compiles',
       inherit: [],
       code: `const tree=new BinarySearchTree();`,
-      evaluate: `tree;`,
-      expected: `{"head":null,"tail":null}`,
+      evaluate: `Boolean(tree);`,
+      expected: true,
+    },
+    {
+      id: 2,
+      name: 'search an empty tree returns false',
+      inherit: [1],
+      code: ``,
+      evaluate: `tree.search(5);`,
+      expected: false,
+    },
+    {
+      id: 3,
+      name: 'search a tree finds value',
+      inherit: [1],
+      code: `tree.insert(5);`,
+      evaluate: `tree.search(5);`,
+      expected: true,
+    },
+    {
+      id: 4,
+      name: 'search a tree does not find value',
+      inherit: [1],
+      code: `tree.insert(5);`,
+      evaluate: `tree.search(2);`,
+      expected: false,
+    },
+    {
+      id: 5,
+      name: 'complex tree, found (1)',
+      inherit: [1],
+      code: `tree.insert(5);tree.insert(7);tree.insert(6);tree.insert(1);tree.insert(2);`,
+      evaluate: `tree.search(6);`,
+      expected: true,
+    },
+    {
+      id: 6,
+      name: 'complex tree, found (2)',
+      inherit: [1, 5],
+      code: ``,
+      evaluate: `tree.search(1);`,
+      expected: true,
+    },
+    {
+      id: 7,
+      name: 'complex tree, not found (1)',
+      inherit: [1, 5],
+      code: ``,
+      evaluate: `tree.search(8);`,
+      expected: false,
+    },
+    {
+      id: 8,
+      name: 'complex tree, not found (2)',
+      inherit: [1, 5],
+      code: ``,
+      evaluate: `tree.search(4);`,
+      expected: false,
     },
   ],
-  setupCode: '',
+  setupCode: `
+  BinarySearchTree.prototype.insert = function(value) {
+    const thisNode = new BinarySearchTreeNode(value);
+    if (!this.root) {
+      this.root = thisNode;
+    } else {
+      let currentRoot = this.root;
+      while (true) {
+        if (currentRoot.value > value) {
+          if (currentRoot.left != null) {
+            currentRoot = currentRoot.left;
+          } else {
+            currentRoot.left = thisNode;
+            break;
+          }
+        } else if (currentRoot.value < value) {
+          if (currentRoot.right != null) {
+            currentRoot = currentRoot.right;
+          } else {
+            currentRoot.right = thisNode;
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+    }
+  };
+  `,
   category: BINARY_SEARCH_TREE,
   type: DATA_STRUCTURE,
   difficulty: BEGINNER,
