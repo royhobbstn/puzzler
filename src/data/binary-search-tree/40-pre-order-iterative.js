1456117a6ad9931b7beda49c42d63fdff7d2f21d
Daniel Trone <danieljtrone@gmail.com>
2021-01-30 09:28:02 -0600

    first recursal

--- src/data/binary-search-tree/40-pre-order-iterative.js
+++ src/data/binary-search-tree/40-pre-order-iterative.js
@@ -12,7 +12,10 @@ const solution = [
   { stage: 0, text: 'class BinarySearchTree {' },
   { stage: 0, text: '  constructor() {' },
   { stage: 0, text: '    this.root = null;' },
+  { stage: 0, text: '    this.root = null;' },
   { stage: 0, text: '  }' },
+  { stage: 0, text: '' },
+  { stage: -1, text: '// IMPLEMENTED  callback(value)' },
   { stage: 0, text: '' },
   { stage: 0, text: '  traversePreOrderIterative(node = this.root, callback = this.callback) {' },
   { stage: 1, text: '    const nodeStack = [];' },
@@ -36,17 +39,25 @@ const solution = [
 
 export const data = {
   problemID: 40,
-  problemName: 'Implement `traversePreOrderIterative` in a Binary Search Tree',
+  problemName: 'Implement **traversePreOrderIterative** in a *BinarySearchTree* class.',
   problemText:
-    'Write a **traversePreOrderIterative** method in a BinarySearchTree Class that traverses a tree **iteratively** and runs the supplied `callback` function on each node value.',
+    'Write a **traversePreOrderIterative** method in a *BinarySearchTree* Class that traverses a tree iteratively and runs the supplied `callback` function on each node value.',
   testCases: [
     {
       id: 1,
       name: 'compiles',
       inherit: [],
       code: `const tree=new BinarySearchTree();`,
-      evaluate: `tree;`,
-      expected: `{"head":null,"tail":null}`,
+      evaluate: `Boolean(tree);`,
+      expected: true,
+    },
+    {
+      id: 2,
+      name: 'proper pre-order ranking',
+      inherit: [1],
+      code: `tree.insert(5);tree.insert(3);tree.insert(11);tree.insert(0);tree.insert(7);tree.insert(2);tree.traversePreOrderIterative();`,
+      evaluate: `JSON.stringify(tree.tempNodeList);`,
+      expected: JSON.stringify([5, 3, 0, 2, 11, 7]),
     },
   ],
   setupCode: `
@@ -56,6 +67,33 @@ export const data = {
   };
   BinarySearchTree.prototype.clearTempNodeList = function (value) {
     BinarySearchTree.prototype.tempNodeList = [];
+  };
+  BinarySearchTree.prototype.insert = function(value) {
+    const thisNode = new BinarySearchTreeNode(value);
+    if (!this.root) {
+      this.root = thisNode;
+    } else {
+      let currentRoot = this.root;
+      while (true) {
+        if (currentRoot.value > value) {
+          if (currentRoot.left != null) {
+            currentRoot = currentRoot.left;
+          } else {
+            currentRoot.left = thisNode;
+            break;
+          }
+        } else if (currentRoot.value < value) {
+          if (currentRoot.right != null) {
+            currentRoot = currentRoot.right;
+          } else {
+            currentRoot.right = thisNode;
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+    }
   };
   `,
   category: BINARY_SEARCH_TREE,
