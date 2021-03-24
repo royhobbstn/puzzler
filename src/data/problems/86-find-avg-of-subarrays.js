bc841028ff120db4f6c1461587eba6e465377dc4
Daniel Trone <danieljtrone@gmail.com>
2021-03-24 06:22:08 -0500

    comment 86 91

--- src/data/problems/86-find-avg-of-subarrays.js
+++ src/data/problems/86-find-avg-of-subarrays.js
@@ -2,15 +2,28 @@ import { ALGORITHM, SLIDING_WINDOW } from '../constants.js';
 
 const solution = [
   { stage: 0, text: '' },
-  { stage: 0, text: 'function find_averages_of_subarrays(K, arr) {' },
+  { stage: 0, text: 'function find_averages_of_subarrays(k, arr) {' },
   { stage: 1, text: '  const result = [];' },
-  { stage: 2, text: '  let windowSum = 0.0;' },
+  { stage: -2, text: '  // sum of numbers in current window' },
+  { stage: 2, text: '  let windowSum = 0;' },
+  { stage: -2, text: '  // window start index' },
   { stage: 2, text: '  let windowStart = 0;' },
   { stage: 3, text: '  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {' },
+  { stage: -4, text: '    // add next number to sum' },
   { stage: 4, text: '    windowSum += arr[windowEnd];' },
-  { stage: 5, text: '    if (windowEnd >= K - 1) {' },
-  { stage: 6, text: '      result.push(windowSum / K);' },
+  {
+    stage: -5,
+    text:
+      '    // only when index of windowEnd reaches kth number do we begin pushing to result array',
+  },
+  { stage: 5, text: '    if (windowEnd >= k - 1) {' },
+  { stage: 6, text: '      result.push(windowSum / k);' },
+  { stage: -7, text: '      // subtract item at beginning of window from sum' },
   { stage: 7, text: '      windowSum -= arr[windowStart];' },
+  {
+    stage: -7,
+    text: '      // increment start of window so it no longer includes subtracted item',
+  },
   { stage: 7, text: '      windowStart += 1;' },
   { stage: 5, text: '    }' },
   { stage: 3, text: '  }' },
@@ -23,7 +36,7 @@ const solution = [
 export const data = {
   problemID: 86,
   problemName: `Find Average of Subarrays`,
-  problemText: `Given an array, find the average of all contiguous subarrays of size ‘K’ in it.`,
+  problemText: `Given an array, find the average of all contiguous subarrays of size ‘k’ in it.`,
   testCases: [
     {
       id: 1,
