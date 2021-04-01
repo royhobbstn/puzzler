989021953d5182899fe24e2ffbc42d255924b113
Daniel Trone <danieljtrone@gmail.com>
2021-04-01 07:44:25 -0500

    two pointers problems

--- src/data/problems/160-triplets-with-smaller-sum.js
+++ src/data/problems/160-triplets-with-smaller-sum.js
@@ -35,7 +35,7 @@ const solution = [
 export const data = {
   problemID: 160,
   problemName: `Triplets with Smaller Sum`,
-  problemText: `Given an array arr of unsorted numbers and a target sum, count all triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three different indices. Write a function to return the count of such triplets.`,
+  problemText: `Given an array \`arr\` of unsorted numbers and a \`target\` sum, count all triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three different indices. Write a function to return the count of such triplets.`,
   testCases: [
     {
       id: 1,
